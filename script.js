4aa199c990f2b7e1a87652f6a8e58ed440ae1c61
Tanner Oliver <435.tanner@gmail.com>
2021-09-19 11:18:29 -0500

    adding arrays to global variables

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-var numbers = "0123456789";
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", ";"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 /*
 Pseudocoding
 
@@ -88,6 +88,8 @@ function buildPassword(numberOfCharacters, getNumbers, getSpecial, getLower, get
 
   if (getUpper === true) {allCharacters = allCharacters.concat(uppercase)};
   console.log(allCharacters);
+
+  //if (numberOfCharacters >= 8 && numberOfCharacters <= 128)
 };
 
 // Write password to the #password input
